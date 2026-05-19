fabe4b17d928725b33b6c84632131c84fdd50405
shaznashafi12 <shaznashafi55@gmail.com>
2026-05-19 21:49:08 +0530

    frs

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 // api.js
 import axios from "axios";
 
-const API_URL = "http://localhost:4000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 // ------------------- USER AUTH -------------------
 // Register
